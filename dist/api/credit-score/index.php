eeebf76cdbfd012429189a6274f3041a2042ee44
mdeller-ping <mdeller@pingidentity.com>
2021-09-07 07:32:43 -0400

    update

---
+++ dist/api/credit-score/index.php
@@ -0,0 +1,14 @@
+<?php
+    header('Content-Type: application/json');
+
+    $seedNumber = rand (320, 830);
+    $rangeHigh = $seedNumber + 20;
+    $rangeLow = $seedNumber - 20;
+
+    $myObj->TransUnionScore = rand ($rangeLow, $rangeHigh);
+    $myObj->ExperianScore = rand ($rangeLow, $rangeHigh);
+    $myObj->EquifaxScore = rand ($rangeLow, $rangeHigh);
+    $myJSON = json_encode($myObj);
+    
+    echo $myJSON;
+?>
